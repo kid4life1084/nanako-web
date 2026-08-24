b0c2ca6a88fb430de2370e7e79e595c64857ff73
kid4life1084 <46184230+kid4life1084@users.noreply.github.com>
2026-08-24 15:07:29 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// Nanako v9.4: service worker intentionally disabled during Omni stabilization.
-self.addEventListener("install",()=>self.skipWaiting());
-self.addEventListener("activate",event=>event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));await self.registration.unregister();})()));
-self.addEventListener("fetch",()=>{});
